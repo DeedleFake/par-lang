8a1e7dff3fbdee6cd6dee7e2e5ed75b3dcf813eb
DeedleFake <deedlefake@users.noreply.github.com>
2025-12-05 14:30:46 -0500

    builtin: implement more of `Os.Process`

--- src/par/builtin/os.rs
+++ src/par/builtin/os.rs
@@ -588,44 +588,46 @@ async fn pathbuf_from_os_path(mut handle: Handle) -> PathBuf {
 }
 
 async fn provide_envmap(handle: Handle) {
-    handle.provide_box(async move |mut handle| {
-        match handle.case().await.as_str() {
-            "size" => {
-                return handle.provide_nat(BigInt::from(std::env::vars_os().count()));
-            }
-            "keys" => {
-                for (name, _) in std::env::vars_os() {
-                    handle.signal(literal!("item"));
-                    handle.send().provide_bytes(os_to_bytes(&name));
+    handle.provide_box(move |mut handle| {
+        async {
+            match handle.case().await.as_str() {
+                "size" => {
+                    return handle.provide_nat(BigInt::from(std::env::vars_os().count()));
                 }
-                handle.signal(literal!("end"));
-                return handle.break_();
-            }
-            "list" => {
-                for (name, value) in std::env::vars_os() {
-                    handle.signal(literal!("item"));
-                    let mut pair = handle.send();
-                    pair.send().provide_bytes(os_to_bytes(&name));
-                    pair.provide_bytes(os_to_bytes(&value));
+                "keys" => {
+                    for (name, _) in std::env::vars_os() {
+                        handle.signal(literal!("item"));
+                        handle.send().provide_bytes(os_to_bytes(&name));
+                    }
+                    handle.signal(literal!("end"));
+                    return handle.break_();
                 }
-                handle.signal(literal!("end"));
-                return handle.break_();
-            }
-            "get" => {
-                let name = handle.receive().bytes().await;
-                let name_os: &OsStr = unsafe { OsStr::from_encoded_bytes_unchecked(name.as_ref()) };
-                match std::env::var_os(name_os) {
-                    Some(val) => {
-                        handle.signal(literal!("ok"));
-                        return handle.provide_bytes(os_to_bytes(&val));
-                    },
-                    None => {
-                        handle.signal(literal!("err"));
-                        return handle.break_();
+                "list" => {
+                    for (name, value) in std::env::vars_os() {
+                        handle.signal(literal!("item"));
+                        let mut pair = handle.send();
+                        pair.send().provide_bytes(os_to_bytes(&name));
+                        pair.provide_bytes(os_to_bytes(&value));
+                    }
+                    handle.signal(literal!("end"));
+                    return handle.break_();
+                }
+                "get" => {
+                    let name = handle.receive().bytes().await;
+                    let name_os: &OsStr = unsafe { OsStr::from_encoded_bytes_unchecked(name.as_ref()) };
+                    match std::env::var_os(name_os) {
+                        Some(val) => {
+                            handle.signal(literal!("ok"));
+                            return handle.provide_bytes(os_to_bytes(&val));
+                        },
+                        None => {
+                            handle.signal(literal!("err"));
+                            return handle.break_();
+                        }
                     }
                 }
+                _ => unreachable!(),
             }
-            _ => unreachable!(),
         }
     });
 }
@@ -670,7 +672,7 @@ async fn provide_command(mut handle: Handle, mut cmd: std::process::Command) {
                match cmd.spawn() {
                    Ok(child) => {
                        handle.signal(literal!("ok"));
-                       return provide_process(handle, child).await;
+                       return provide_process(handle, Arc::new(Mutex::new(child))).await;
                    }
                    Err(err) => {
                        handle.signal(literal!("err"));
@@ -699,35 +701,61 @@ async fn provide_command(mut handle: Handle, mut cmd: std::process::Command) {
     }
 }
 
-async fn provide_process(mut handle: Handle, child: std::process::Child) {
-    let child = Arc::new(Mutex::new(child));
+async fn provide_process(mut handle: Handle, child: Arc<Mutex<std::process::Child>>) {
+    loop {
+        match handle.case().await.as_str() {
+            "info" => {
+                provide_processinfo(handle.send(), child.clone());
+                continue;
+            }
+            "wait" => {
+                match child.lock().unwrap().wait() {
+                    Ok(status) => {
+                        handle.signal(literal!("ok"));
+                        match status.code() {
+                            Some(code) => {
+                                handle.signal(literal!("ok"));
+                                return handle.provide_int(BigInt::from(code));
+                            }
+                            None => {
+                                handle.signal(literal!("err"));
+                                return handle.break_();
+                            }
+                        }
+                    }
+                    Err(err) => {
+                        handle.signal(literal!("err"));
+                        return handle.provide_string(ParString::from(err.to_string()));
+                    }
+                }
+            }
+            "kill" => {
+                match child.lock().unwrap().kill() {
+                    Ok(()) => {
+                        handle.signal(literal!("ok"));
+                        return handle.break_();
+                    }
+                    Err(err) => {
+                        handle.signal(literal!("err"));
+                        return handle.provide_string(ParString::from(err.to_string()));
+                    }
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn provide_processinfo(handle: Handle, child: Arc<Mutex<std::process::Child>>) {
     handle.provide_box(move |mut handle| {
         let child = child.clone();
         async move {
             match handle.case().await.as_str() {
-                "wait" => {
-                    match child.lock().unwrap().wait() {
-                        Ok(status) => {
-                            handle.signal(literal!("ok"));
-                            match status.code() {
-                                Some(code) => {
-                                    handle.signal(literal!("ok"));
-                                    return handle.provide_int(BigInt::from(code));
-                                }
-                                None => {
-                                    handle.signal(literal!("err"));
-                                    return handle.break_();
-                                }
-                            }
-                        }
-                        Err(err) => {
-                            handle.signal(literal!("err"));
-                            return handle.provide_string(ParString::from(err.to_string()));
-                        }
-                    }
+                "id" => {
+                    return handle.provide_nat(BigInt::from(child.lock().unwrap().id()));
                 }
                 _ => unreachable!(),
             }
         }
-    })
+    });
 }
